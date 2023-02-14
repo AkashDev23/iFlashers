89608291760435c615dcae4fdf2d35df326ebdae
akash dev <akash.dev.8092@gmail.com>
2023-02-14 21:27:42 +0530

    first commit

---
+++ app/src/main/java/com/example/iflashers/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.iflashers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity<LeDeviceListAdapter> extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+    private boolean scanning;
+    private Handler handler = new Handler();
+
+    //Stops scanning after 10 seconds
+    private static final long SCAN_PERIOD = 10000;
+
+    private void scanLeDevice() {
+        if (!scanning) {
+            //stops scanning after a predefined scan period
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    scanning = false;
+                    bluetoothLeScanner.stopScan(leScanCallback);
+                }
+            }, SCAN_PERIOD);
+            scanning = true;
+            bluetoothLeScanner.startScan(leScanCallback);
+        } else {
+            scanning = false;
+            bluetoothLeScanner.stopScan(leScanCallback);
+        }
+    }
+
+    private LeDeviceListAdapter leDeviceListAdapter = new LeDeviceListAdapter();
+
+    // Device scan callback.
+    private ScanCallback leScanCallback =
+            new ScanCallback() {
+                @Override
+                public void onScanResult(int callbackType, ScanResult result) {
+                    super.onScanResult(callbackType, result);
+                    leDeviceListAdapter.addDevice(result.getDevice());
+                    leDeviceListAdapter.notifyDataSetChanged();
+                }
+            };
+    bluetoothGatt =device.connectGatt(this,false,gattCallback);
+}
